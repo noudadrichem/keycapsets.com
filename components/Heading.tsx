9b57c1db8ddd381a126489c4cc7a07d83482ad0b
noudadrichem <info@noudadrichem.com>
2020-01-20 14:02:02 +0100

    init

---
+++ components/Heading.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Heading(props) {
+    return (<header>header</header>)
+}
+
+export default Heading;
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import '../assets/styles/main.scss';
+
+import Heading from '../components/Heading';
+
+interface HomeProps {
+
+}
+
+function Home(props: HomeProps) {
+
+    useEffect(() => {
+
+    });
+
+    return (
+        <div id="home">
+            <Heading />
+        </div>
+    )
+}
+
+export default Home;
