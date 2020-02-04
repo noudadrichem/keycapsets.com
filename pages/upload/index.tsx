0f553375a233b15fc18e7ef5f7687e8a8e0150b1
noudadrichem <info@noudadrichem.com>
2020-02-04 18:23:28 +0100

    upload page styling

--- pages/upload/index.tsx
+++ pages/upload/index.tsx
@@ -2,18 +2,33 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 import '../../assets/styles/main.scss';
+import '../../assets/styles/upload.scss';
 
 import withData from '../../hooks/withData';
+import Heading from '../../components/Heading';
 
-interface HomeProps { }
+interface UploadProps {}
 
-function Home(props: HomeProps) {
+function Upload(props: UploadProps) {
     return (
-        <>
-            <Link href="/upload/set">Set</Link> <br /><br />
-            <Link href="/upload/vendor">Vendor</Link>
-        </>
+        <div className="container upload">
+
+            <Heading mainTitle="Upload" subTitle="Make yourself known!" left />
+
+
+            <div className="cards">
+                <div className="card">
+                    <Link  href="/upload/set">Here to add a set?</Link>
+                </div>
+
+                <div className="card">
+                    <Link  href="/upload/vendor">Are you a vendor?</Link>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
-export default withData(Home);
+export default withData(Upload);
