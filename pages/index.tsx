ed4b3a6cf4f3d88ac5ec7ff8493c2734fdb4b0dd
noudadrichem <info@noudadrichem.com>
2020-03-20 09:26:09 +0100

    fix lazy fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ function Home(props: HomeProps) {
         }
         if (isAtBottomOfPage) {
             setLoadingExtra(true);
-            // fetchMoreWhenBottomOfPage();
+            fetchMoreWhenBottomOfPage();
             setIsAtBottomOfPage(false);
         }
     }, [isAtBottomOfPage])
