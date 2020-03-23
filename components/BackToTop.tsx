23fdb2eec1a0d6218173278b4ad81e37d382438c
Felix Volny <23100@ma-web.nl>
2020-03-23 21:24:44 +0100

    Add BackToTop component

---
+++ components/BackToTop.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import useWindowScroll from '../hooks/useWindowScroll';
+import Arrow from './Arrow';
+import Button from './Button';
+
+function BackToTop(): JSX.Element {
+
+    const isBrowser = typeof window !== `undefined`;
+    const [isPastOffset, setIsPastOffset] = useState(false);
+    const { y } = useWindowScroll();
+
+    useEffect(function updateScrollOffset() {
+      if (isBrowser) {
+        const innerHeight = window.innerHeight;
+        setIsPastOffset(y > innerHeight);
+      }
+    })
+
+    function scrollToTop() {
+      const speed = 2000;
+      const easing = pos => Math.sin(pos * (Math.PI / 2));
+      let currentTime = 0;
+
+      const time = Math.max(
+        0.1,
+        Math.min(y / speed, 0.8)
+      )
+
+      function tick() {
+        currentTime += 1 / 60;
+
+        const p = currentTime / time;
+        const t = easing(p);
+
+        if (p < 1) {
+          window.requestAnimationFrame(tick);
+
+          window.scrollTo(0, y + (0 - y) * t);
+        } else {
+          window.scrollTo(0, 0);
+        }
+      }
+
+      tick();
+    }
+
+    return (
+      <Button
+          onClick={() => scrollToTop()}
+          variant="secondary"
+          className={`back-to-top ${isPastOffset ? 'back-to-top-show' : ''}`}>
+           <Arrow direction="top" />
+      </Button>
+    )
+}
+
+export default BackToTop
