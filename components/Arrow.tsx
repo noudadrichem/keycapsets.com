57afc5ed3b2bdaf4db9b7b35904b7a0abe1f6cee
Felix Volny <23100@ma-web.nl>
2020-03-23 21:23:34 +0100

    Add direction prop to Arrow component

--- components/Arrow.tsx
+++ components/Arrow.tsx
@@ -3,16 +3,18 @@ import React from 'react';
 interface ArrowProps {
     size?: Number;
     color?: string;
+    direction?: string;
 }
 
 function Arrow(props: ArrowProps): JSX.Element {
     const {
       size,
       color,
+      direction
     } = props;
 
     return (
-        <svg className="arrow" xmlns='http://www.w3.org/2000/svg' width={size.toString()} height={size.toString()} viewBox='0 0 24 24' fill='none' stroke={color}
+        <svg className={`arrow arrow-${direction}`} xmlns='http://www.w3.org/2000/svg' width={size.toString()} height={size.toString()} viewBox='0 0 24 24' fill='none' stroke={color}
             strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
             <path d='M5 12h13M12 5l7 7-7 7' /></svg>
     );
@@ -22,6 +24,7 @@ function Arrow(props: ArrowProps): JSX.Element {
 Arrow.defaultProps = {
   size: 24,
   color: '#539BFB',
+  direction: 'right'
 }
 
 export default Arrow;
