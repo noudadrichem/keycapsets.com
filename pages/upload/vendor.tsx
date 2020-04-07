c30a40e419471bc1c0ca3c7fb1dfe1b0303ef771
noudadrichem <info@noudadrichem.com>
2020-04-07 21:19:16 +0200

    country and continent for vendor upload

--- pages/upload/vendor.tsx
+++ pages/upload/vendor.tsx
@@ -1,3 +1,5 @@
+import path from 'path';
+import fs from 'fs';
 import React, { useEffect, useState } from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import withGA from 'next-ga';
@@ -15,28 +17,38 @@ import Heading from '../../components/Heading';
 import Footer from '../../components/Footer';
 import Nav from '../../components/Nav';
 import Meta from '../../components/Meta';
+import Multiselect from '../../components/Multiselect';
 
-interface UploadVendorProps { }
+interface UploadVendorProps {
+    countries: any[];
+    continents: any[];
+}
 
 function UploadVendor(props: UploadVendorProps) {
+    const { countries, continents } = props;
     const [nameValue, nameInput, setName] = useInput({ label: 'Name:'});
-    const [countryValue, countryInput, setCountry] = useInput({ label: 'Country:'});
+    const [countryValue, setCountry] = useState<any>({ label: 'Netherlands', value: 'NL' });
+    const [continentValue, setContinent] = useState<any>({ label: "Europe", value: "EU" });
     const [logoUrlValue, logoUrlInput, setLogoInput] = useInput({ label: 'Logo url:'});
     const [urlValue, urlInput, setUrl] = useInput({ label: 'Website address:'});
-    const [socials, setSocials] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [shouldReset, setShouldReset] = useState(false);
+    const [socials, setSocials] = useState<any[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [shouldReset, setShouldReset] = useState<boolean>(false);
+    const [addVendor, mutationResponse] = useMutation<string>(CREATE_VENDOR_MUTATION);
 
-    const [addVendor, mutationResponse] = useMutation(CREATE_VENDOR_MUTATION);
+    useEffect(() => {
+        console.log({ countries, continents })
+    }, [])
+
     async function uploadVendor() {
         const variables = {
             name: nameValue,
-            country: countryValue,
+            country: `${continentValue.value}-${countryValue.value}`,
             logoUrl: logoUrlValue,
             url: urlValue,
             socials
         };
-
+        console.log(variables);
         const result = await addVendor({ variables });
         console.log('result', result);
 
@@ -66,7 +78,20 @@ function UploadVendor(props: UploadVendorProps) {
                 <div className="grid-container">
                     <div className="column">
                         {nameInput}
-                        {countryInput}
+
+                        <Multiselect
+                            label="Country"
+                            onChange={(selectedCountry: any) => setCountry(selectedCountry)}
+                            options={countries}
+                            defaultValue={{ label: 'Netherlands', value: 'NL'}}
+                        />
+                        <Multiselect
+                            label="Continents"
+                            onChange={(selectedContinent: any) => setContinent(selectedContinent)}
+                            options={continents}
+                            defaultValue={{ label: "Europe", value: "EU"}}
+                        />
+
                         {logoUrlInput}
                         {urlInput}
                         <MultipleInputs
@@ -94,6 +119,42 @@ function UploadVendor(props: UploadVendorProps) {
             </div>
         </>
     )
+}
+
+export async function getStaticProps() {
+    const filepath = process.cwd() + '/assets/countries.json';
+    const fileContents = fs.readFileSync(filepath, 'utf8')
+    const countries = JSON.parse(fileContents);
+
+    const countriesFormatted: any[] = countries.map((country: any) => {
+        return {
+            label: country.countryName,
+            value: country.twoLetterCountryCode
+        }
+    });
+
+    const continentsFormatted = countries
+        .reduce((res, country) => {
+            if (!res[1].includes(country.continentCode)) {
+                res[1].push(country.continentCode);
+                res[0].push(country);
+            }
+            return res;
+        }, [[],[]])[0]
+        .map((country: any) => {
+            return {
+                label: country.continentName,
+                value: country.continentCode
+            }
+        });
+
+
+    return {
+        props: {
+            countries: countriesFormatted,
+            continents: continentsFormatted
+        }
+    }
 }
 
 export default withGA('UA-115865530-2', Router)(withData(UploadVendor));
