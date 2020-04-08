9a9e469e51baee6ac00300499eb3ef19346f1e3c
Grant Glidewell <ggcadc@gmail.com>
2020-04-08 11:42:18 -0700

    revert quote formatting in context

--- context.ts
+++ context.ts
@@ -1,27 +1,27 @@
-import { createContext } from "react";
-import moment from "moment";
-import { InititalState } from "typings";
-import { PROFILE_OPTIONS } from "./constants";
+import { createContext } from 'react';
+import moment from 'moment';
+import { InititalState } from 'typings';
+import { PROFILE_OPTIONS } from './constants';
 
 export function reduceState(state, obj) {
     const reducedState = {
         ...state,
         ...obj,
     };
-    if (process.env.NODE_ENV === "development") {
-        console.log(moment().format("hh:mm:ss") + "_STATE...", reducedState);
+    if (process.env.NODE_ENV === 'development') {
+        console.log(moment().format('hh:mm:ss') + '_STATE...', reducedState);
     }
     return reducedState;
 }
 
 export const INITITAL_STATE: InititalState = {
-    activeTab: "all",
-    availabilityFilter: "none",
-    tabs: ["all", "gmk", "pbt", "sa", "dsa", "kat", "jtk", "kam", "dcs"],
-    availability: ["ic", "waiting", "gb", "ended"],
+    activeTab: 'all',
+    availabilityFilter: 'none',
+    tabs: ['all', 'gmk', 'pbt', 'sa', 'dsa', 'kat', 'jtk', 'kam', 'dcs'],
+    availability: ['ic', 'waiting', 'gb', 'ended'],
     // tabs: PROFILE_OPTIONS.map(o => o.label),
     keycapsets: [],
-    searchQuery: "",
+    searchQuery: '',
     allKeycapsetsCount: 0,
     setGlobalState: () => {},
 };
