f44c2850a46703904a5842f7f17065551ae9006f
r.george <r.george@socialblue.com>
2020-04-08 22:03:19 +0200

    Add discord href to discord-banner

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -12,7 +12,7 @@ function Nav(props: NavProps): JSX.Element {
 
     return (
         <nav className="nav">
-            <a className="discord-banner">
+            <a className="discord-banner" href="https://discord.gg/dq8cyMS">
                 <img src="/images/discord.svg" /> Join the conversation on our discord!
             </a>
             <div className={`container ${isLargeContainer ? 'large' : ''}`}>
