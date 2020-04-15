e72d10d2951d8df59da6ec4e5e646697cb90dd3d
TeroonsiMcKooni <43268243+rafaeldatarock@users.noreply.github.com>
2020-04-15 16:58:34 +0200

    Vendors sorted by continent

--- pages/vendors.tsx
+++ pages/vendors.tsx
@@ -1,30 +1,32 @@
 import React, { useState, useEffect, useContext } from 'react';
 import Error from 'next/error';
-import { useMutation, useQuery } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 import withGA from 'next-ga';
 import Router from 'next/router';
 
-import { GET_VENDORS_QUERY } from '../queries';
-
 import withData from '../hooks/withData';
+import Context from '../context';
+import { GET_VENDORS_QUERY } from '../queries';
+import { Vendor } from 'typings';
 
 import Heading from '../components/Heading';
 import Footer from '../components/Footer';
-
-import '../assets/styles/main.scss';
-import { Vendor } from 'typings';
 import Nav from '../components/Nav';
 import LoadingKeyboard from '../components/LoadingKeyboard';
 import VendorCard from '../components/VendorCard';
 import Meta from '../components/Meta';
-import Head from 'next/head';
-import Context from '../context';
 import CTACard from '../components/CTACard';
+
+import '../assets/styles/main.scss';
+import '../assets/styles/vendors.scss';
+import countryIsoList from '../assets/countries';
+import Head from 'next/head';
 
 interface VendorProps {}
 
 function Vendors(props: VendorProps): JSX.Element {
     const { loading, error, data } = useQuery(GET_VENDORS_QUERY);
+    const vendors = data.vendors;
 
     if (loading) {
         return <LoadingKeyboard />;
@@ -35,39 +37,89 @@ function Vendors(props: VendorProps): JSX.Element {
         return <Error title="Oops, small mistake here..." statusCode={502} />;
     }
 
+    function generateAccumArray(len) {
+        let arr = []
+        for(let i = 0; i < len; i++) {
+            arr.push([])
+        }
+        return arr
+    }
+
+    function getLabelByContinentCode(code) {
+        let label = ''
+        const [continent, country] = code.split('-')
+
+        countryIsoList.forEach(countryIso => {
+            if (continent === 'ALL') {
+                label = 'Worldwide'
+            }
+            if (countryIso.continentCode === continent) {
+                label = countryIso.continentName
+            }
+        });
+        return label
+    }
+
+    // make list of unique present vendor continents
+    const availableContinents = vendors.map(v => v.country).reduce((res, country) => {
+        const continentCode = country.split('-')[0]
+        const [con1, con2] = continentCode.split(',')
+
+        if (!res.includes(con1)) {
+           res.push(con1)
+        }
+        if (!res.includes(con2)) {
+            res.push(con2)
+        }
+
+        return res;
+    }, [])
+
+    let totalAcc = generateAccumArray(availableContinents.length)
+
+    // filter all vendor objects by key 'country', push all vendors to dedicated continent array
+    const filteredVendorLists = vendors.reduce((accum, vendor) => {
+        availableContinents.map((continent, jdx) => {
+            // if 2 continentcodes are listed, the vendor get put in the section of the continentcode that's listed first
+            const continentCode = vendor.country.split('-')[0]
+            const con1 = continentCode.split(',')[0]
+
+            if (con1 === continent) {
+                accum[jdx].push(vendor)
+            }
+        })
+
+        return accum
+    }, totalAcc)
+
+    var sortedVendors =  filteredVendorLists.reduce(function(sortedVendors, field, index) {
+        sortedVendors[availableContinents[index]] = field;
+        return sortedVendors;
+    }, {})
+
     return (
         <>
             <Meta />
-
             <Nav />
             <div className="container">
                 <Heading mainTitle="Vendors" subTitle="Available vendors." />
+                {
+                    Object.keys(sortedVendors).map(key => (
+                        <div className="continent-section">
+                            <div className="continent-bar">
+                                <h2 className={'no-bold left'}> { getLabelByContinentCode(key) } </h2>
+                            </div>
 
-                <h2 className={'no-bold center'}>Europe</h2>
-                <div className="images-container">
-                    {/* REGION EU */}
-                    {
-                        data.vendors
-                            .map((vendor: Vendor) => {
-                                    if (vendor.country.includes('EU')) {
-                                        return <VendorCard vendor={vendor} key={vendor._id}/>;
-                                    }
+                            <div className="images-container">
+                                {
+                                    sortedVendors[key].map((vendor: Vendor) => (
+                                        <VendorCard vendor={vendor} key={vendor._id}/>
+                                    ))
                                 }
-                            )
-                    }
-                </div>
-
-                <div className="images-container">
-                    {/* UNFILTERED */}
-                    {
-                        data.vendors
-                            .map((vendor: Vendor) => {
-                                    console.log(vendor.name,': ',vendor.country);
-                                    return <VendorCard vendor={vendor} key={vendor._id}/>;
-                                }
-                            )
-                    }
-                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
             <Footer />
             <CTACard />
