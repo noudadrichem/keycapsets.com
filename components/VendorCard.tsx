b199bb196b50c0f490cdd018181f243c2d59145a
TeroonsiMcKooni <43268243+rafaeldatarock@users.noreply.github.com>
2020-04-16 12:01:38 +0200

    Fixed PR #15 by using CSS only & added anchor tag to make whole vendor card link to vendorUrl

--- components/VendorCard.tsx
+++ components/VendorCard.tsx
@@ -12,30 +12,32 @@ function VendorCard(props: VendorCardProps): JSX.Element {
     const { name, logoUrl, url } = vendor;
 
     return (
-        <div className="vendor-card">
-            <div className="image">
-                <img
-                    src={
-                        logoUrl === undefined || logoUrl === ''
-                            ? `https://via.placeholder.com/400x300/f2f2f2?text=${name}`
-                            : logoUrl
-                    }
-                    alt={name}
-                />
-            </div>
+        <a href={url} target="_blank">
+            <div className="vendor-card">
+                <div className="image">
+                    <img
+                        src={
+                            logoUrl === undefined || logoUrl === ''
+                                ? `https://via.placeholder.com/400x300/f2f2f2?text=${name}`
+                                : logoUrl
+                        }
+                        alt={name}
+                    />
+                </div>
 
-            <div className="details">
-                <div className="horizontal">
-                    <div className="left">
-                        <h4>{name}</h4>
-                    </div>
+                <div className="details">
+                    <div className="horizontal">
+                        <div className="left">
+                            <h4>{name}</h4>
+                        </div>
 
-                    <div className="right">
-                        <ButtonLink href={url}>Go to vendor</ButtonLink>
+                        <div className="right">
+                            <ButtonLink href={url}>Go to vendor</ButtonLink>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </a>
     );
 }
 
