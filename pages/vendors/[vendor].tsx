98836525e849ba98beed6fdc37ce45ec12e3fd85
Felix Volny <23100@ma-web.nl>
2020-04-20 21:06:51 +0200

    Add Keycaps to vendor single page

--- pages/vendors/[vendor].tsx
+++ pages/vendors/[vendor].tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import moment from 'moment';
+import React, { useEffect, useState } from 'react';
+import Context from '../../context';
+import { useApolloClient } from '@apollo/react-hooks';
 import { useRouter, Router } from 'next/router';
-import Head from 'next/head';
 import { useQuery } from '@apollo/react-hooks';
 import { Keycapset, Vendor } from 'typings';
 import withGA from 'next-ga';
-
-import withData from '../../hooks/withData'
+import withData from '../../hooks/withData';
+import countryIsoList from '../../assets/countries';
 import { GET_SINGLE_VENDOR_QUERY, GET_SETS_BY_VENDOR } from '../../queries';
 
 import Footer from '../../components/Footer';
@@ -14,22 +14,38 @@ import Heading from '../../components/Heading';
 import Nav from '../../components/Nav';
 import LoadingKeyboard from '../../components/LoadingKeyboard';
 import ButtonLink from '../../components/ButtonLink';
-import Images from '../../components/Images';
-import Meta from '../../components/Meta';
+import Socials from '../../components/Socials';
+import ImageCard from '../../components/ImageCard';
 
 interface SetProps {}
 
 function VendorPage(props: SetProps) {
     const router = useRouter();
+    const client = useApolloClient();
+    const [vendorSets, setVendorSets] = useState([]);
     const { vendor: slug } = router.query;
-
-    console.log({ slug })
-    let variables = { slug }
-    console.log(variables);
+    const variables = { slug };
 
     const { loading, error, data } = useQuery(GET_SINGLE_VENDOR_QUERY, { variables });
 
-    console.log(data);
+    useEffect(() => {
+        (async () => {
+            if (!loading) {
+                const { _id } = data.vendorBySlug;
+                const sets = await fetchVendorSets(_id);
+
+                setVendorSets(sets.data.keycapsetsByVendor);
+            }
+        })();
+    }, [loading, data]);
+
+    async function fetchVendorSets(id: String): Promise<any> {
+        const fetchVendorSetsResult = await client.query({
+            query: GET_SETS_BY_VENDOR,
+            variables: { id },
+        });
+        return fetchVendorSetsResult;
+    }
 
     if (loading) {
         return <LoadingKeyboard />;
@@ -40,48 +56,63 @@ function VendorPage(props: SetProps) {
     }
 
     const vendor: Vendor = data.vendorBySlug;
-    variables = { vendorId: vendor._id }
 
-    const keycapsets = useQuery(GET_SETS_BY_VENDOR, { variables });
+    console.log(vendor);
 
-    // console.log(keycapsets);
+    const vendorRegion = countryIsoList.find((country) => {
+        const _continent = vendor.country.split('-')[0];
+        const _country = vendor.country.split('-')[1];
 
+        if (typeof _country === undefined) {
+            return country.continentCode === _continent;
+        }
 
+        return country.continentCode === _continent && country.twoLetterCountryCode === _country;
+    });
 
-    return vendor !== undefined && (
-      <div className="vendor">
-          <Meta
-              // title={`${set.type.toUpperCase()} Keycapset ${set.name}`}
-              // metaImgUrl={set.coverImageUrl}
-          />
+    return (
+        vendor !== undefined && (
+            <div className="vendor">
+                <Nav />
 
-          <Nav />
+                <div className="container">
+                    <Heading left mainTitle={vendor.name} subTitle={`Good luck with sharing!`} />
 
-          <div className="container">
-              <Heading
-                  left
-                  mainTitle={vendor.name}
-                  subTitle={`Good luck with sharing!`}
-              />
+                    <div className="info-section">
+                        <div>
+                            <img src={vendor.logoUrl} />
+                        </div>
 
-              <div className="info-section">
-                  <div>
-                    <img src={vendor.logoUrl} />
-                  </div>
+                        <div>
+                            <h3>Info</h3>
+                            <p>Name: {vendor.name}</p>
+                            <p>Continent: {vendorRegion?.continentName || '-'}</p>
+                            <p>Country: {vendorRegion?.countryName || '-'}</p>
 
-                  <div>
-                      <h3>Info</h3>
-                  </div>
+                            <Socials socials={vendor.socials} />
 
-              </div>
+                            <ButtonLink isLarge href={vendor.url}>
+                                Visit the website
+                            </ButtonLink>
+                        </div>
+                    </div>
 
-              <Images keycapsets={[]} />
+                    {vendorSets.length > 0 ? (
+                        <div className="vendor__keycapsets">
+                            <h3>Keycapsets</h3>
+                            <div className="images-container">
+                                {vendorSets.map((keycapset: Keycapset) => (
+                                    <ImageCard {...{ keycapset }} key={keycapset._id} />
+                                ))}
+                            </div>
+                        </div>
+                    ) : null}
+                </div>
 
-          </div>
-
-          <Footer />
-      </div>
-    )
+                <Footer />
+            </div>
+        )
+    );
 }
 
 export default withGA('UA-115865530-2', Router)(withData(VendorPage));
