3e6e3071781896c01f5e25b89d57a31c95dac52b
noudadrichem <info@noudadrichem.com>
2020-05-18 15:26:11 +0200

    remove maintenance banner and add sign up teaser

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 
 import Logo from './Logo';
+import Button from './Button';
 
 interface NavProps {
     isLargeContainer?: boolean;
@@ -9,16 +10,17 @@ interface NavProps {
 
 function Nav(props: NavProps): JSX.Element {
     const { isLargeContainer } = props;
+    const isBrowser = typeof window !== `undefined`;
 
     return (
         <nav className="nav">
             {/* <a className="discord-banner" href="https://discord.gg/dq8cyMS">
                 <img src="/images/discord.svg" /> Join the conversation on our discord!
             </a> */}
-            <div className="maintenance-banner">
+            {/* <div className="maintenance-banner">
                 In the weekend of 16th and 17th may I'll be doing some "big" maintenance, this could result in some
                 downtime. I'm sorry in advance. ❤️
-            </div>
+            </div> */}
 
             <div className={`container ${isLargeContainer ? 'large' : ''}`}>
                 <div className="nav-container">
@@ -37,6 +39,12 @@ function Nav(props: NavProps): JSX.Element {
                         <Link href="/about" as="/about">
                             <a>About</a>
                         </Link>
+
+                        {isBrowser && window.innerWidth > 800 && (
+                            <Button size="md" variant="primary" onClick={() => 'Coming soon...'} isDisabled>
+                                Sign up (coming soon)
+                            </Button>
+                        )}
                     </div>
                 </div>
             </div>
