8660e63a7bc7480bff8952c257aa80b093f6d778
TeroonsiMcKooni <43268243+rafaeldatarock@users.noreply.github.com>
2020-05-29 21:12:04 +0200

    Improved sign-up and terms

--- pages/sign-up/google.tsx
+++ pages/sign-up/google.tsx
@@ -18,31 +18,33 @@ function SignUpGoogle() {
                     <h3>Features</h3>
                     <ul className="features">
                         <li>Favorite sets, see your collection. </li>
-                        <li>Create your own collections for e.g. your board.(Coming)</li>
-                        <li>Opt into groupbuy updates via mail (Coming)</li>
-                        <li>Upload a keycapset(Coming (again))</li>
-                        <li>Collect IC votes.(Coming)</li>
-                        <li>Get a full fledged designed page for your set.(Coming)</li>
-                        <li>Sign up to a calender feed.(Coming)</li>
-                        <li>Hook up to vendors to get your MOQ data in 1 place.(Coming)</li>
+                        <li>Create your own collections for e.g. your board. *</li>
+                        <li>Opt into groupbuy updates via mail. *</li>
+                        <li>Upload a keycapset. *</li>
+                        <li>Collect IC votes. *</li>
+                        <li>Get a full fledged designed page for your set. *</li>
+                        <li>Sign up to a calender feed. *</li>
+                        <li>Hook up to vendors to get your MOQ data in one place. *</li>
                     </ul>
                 </div>
 
                 <p className="light small-width alinea">
-                    By creating an account via Google you're able to favorite your keycapsets, have an easy to go
-                    overview of what you like and might want to use for your next build. You could vote on ICs (coming
-                    soon) You could opt in on recieving weekly or monthly updates via email (coming soon).
+                    By creating an account via Google you're able to favorite keycapsets and have a simple overview of
+                    what you like and might want to use for your next build. You could vote on ICs. * You could opt-in
+                    on recieving weekly or monthly updates via email. *
                 </p>
 
                 <p className="light small-width alinea">
-                    Are you a designer? Sign up and upload your sets. Collect IC votes and get a beautiful partly self
-                    designed page to promote your sets on various platforms!(coming soon)
+                    Are you a designer? Sign up and upload your sets. Collect IC votes and get a beautiful (partly self
+                    designed) page to promote your sets on various platforms! *
                 </p>
 
                 <p className="light small-width alinea">
-                    Are you a vendor? Sign up and let users know know you exist! Are you a vendor designing sets?
-                    Obviously all the above apply for you aswell!(comgin soon)
+                    Are you a vendor? Sign up and let users know you exist! Are you a vendor designing sets? Obviously
+                    all the above apply for you aswell! *
                 </p>
+
+                <p className="light small-width alinea">* Feature coming soon!</p>
 
                 <div className="">
                     <Checkbox
