6610529c6212f2321ca6f7ae93002aaeb86fe00c
Felix Volny <23100@ma-web.nl>
2020-06-10 12:19:18 +0200

    Add Kit

---
+++ components/poseidon/Kit.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from 'react';
+
+interface KitProps {
+    name: String;
+    description: String;
+    imgUrl: String;
+}
+
+function Kit(props: KitProps): JSX.Element {
+    const { name, description, imgUrl } = props;
+
+    return (
+        <div className="kit container">
+            <div className="kit__cover">
+                <img src={`${imgUrl}`} />
+            </div>
+            <div className="kit__info">
+                <h3 data-name={name} className="kit__info-name">
+                    {name}
+                </h3>
+                <p className="kit__info-description">{description}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Kit;
