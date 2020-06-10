7bfcdb07a7eb870532bdfa1c7a649bbfd1d5aa8b
Felix Volny <23100@ma-web.nl>
2020-06-10 12:21:44 +0200

    Add data/components to Poseidon page

--- pages/poseidon.tsx
+++ pages/poseidon.tsx
@@ -9,6 +9,10 @@ import 'slick-carousel/slick/slick.css';
 import '../assets/styles/main.scss';
 
 import Meta from '../components/Meta';
+import Hero from '../components/poseidon/Hero';
+import Kit from '../components/poseidon/Kit';
+import Renders from '../components/poseidon/Renders';
+import Section from '../components/poseidon/Section';
 import { ApolloClient } from 'apollo-boost';
 import PoseidonLayout from '../layouts/PoseidonLayout';
 
@@ -29,9 +33,35 @@ function PoseidonPage(props: PoseidonPageProps) {
             <Meta title="Official IC page for GMK Poseidon" description="Noud is tering cool" metaImgUrl="" />
             <div className="set">
                 <Layout>
-                    <div>
-                        <pre>{JSON.stringify(keycapset, undefined, 4)}</pre>
-                    </div>
+                    <Hero />
+
+                    <Section title="Kits">
+                        {keycapset.kits.map((kit) => (
+                            <Kit {...kit} />
+                        ))}
+                    </Section>
+
+                    <Section title="Design & Colors">
+                        <p className="section-description">
+                            This set originates from my love mythology in partially Ancient greek and the ocean, I
+                            started working on this set back in August of 2018, It’s original concept was to run in the
+                            back then the new KAT profile from Keyreative with reserved dyesub, sadly back then reverse
+                            dyesub was still something they were working on and decided to hold off and wait for further
+                            details. After awhile I started rethinking the concept for a different profile and together
+                            with the help of u/Janglad(belgium homo) which who I have worked in the past on project like
+                            Minimal and Equinox BT, we came with the decision to run Poseidon on the beloved Cherry
+                            profile from GMK. as the barebones were already set there was still a lot to do with
+                            researching the possibilities within the GMK profile. Here we chose to go with the more
+                            traditional Icon+Text modifier legends as they were also used on the vintage Greek cherry
+                            keyboards. For the colors we decided to go with GMK stock colors TU1 which is a deep calm
+                            and navy blue and TU2 which is a bright cyan that helps with the contrast for more readable
+                            legends.
+                        </p>
+                    </Section>
+
+                    <Section title="Renders">
+                        <Renders renders={keycapset.imageUrls} />
+                    </Section>
                 </Layout>
             </div>
         </>
@@ -46,7 +76,20 @@ PoseidonPage.getInitialProps = () => {
             type: 'cherry',
             coverImageUrl: 'cover IMG hier',
             vendors: [],
-            imageUrls: [''],
+            imageUrls: [
+                'https://i.imgur.com/dCaIcDa.jpg',
+                'https://i.imgur.com/ipirs43.jpg',
+                'https://i.imgur.com/Cot5qNY.jpg',
+                'https://i.imgur.com/ToZLJ4l.jpg',
+                'https://i.imgur.com/p40MZ9v.jpg',
+                'https://i.imgur.com/PDXTXJ5.jpg',
+                'https://i.imgur.com/45XL6Sh.jpg',
+                'https://i.imgur.com/CIikahD.jpg',
+                'https://i.imgur.com/xmJVe8L.jpg',
+                'https://i.imgur.com/Ois87Lt.jpg',
+                'https://i.imgur.com/KWydW65.jpg',
+                'https://i.imgur.com/YsRZl7H.jpg',
+            ],
             websiteUrl: 'https://keycapsets.com/poseidon',
             groupbuyStartDate: '-',
             groupbuyEndDate: '-',
@@ -57,32 +100,36 @@ PoseidonPage.getInitialProps = () => {
             accentColor3: '0000ff',
             kits: [
                 {
-                    name: 'Base',
-                    description: '',
+                    name: 'Olyumpus',
+                    description:
+                        'The Olympus kit features compatibility for the most popular layouts in the community and includes both esc and enter accent keys.',
                     type: '',
                     price: '',
-                    imgUrl: '',
+                    imgUrl: 'https://i.imgur.com/w8nejQq.jpg',
                 },
                 {
-                    name: 'Novelties',
-                    description: '',
+                    name: 'Titans',
+                    description:
+                        'The Titans kit features both ISO and numpad support, this includes the accent enter for the numpad and 1800 support.',
                     type: '',
                     price: '',
-                    imgUrl: '',
+                    imgUrl: 'https://i.imgur.com/AFG2yvw.jpg',
                 },
                 {
-                    name: 'Spacebars',
-                    description: '',
+                    name: 'Tholos',
+                    description:
+                        'The Tholos kit features support for a variety of keyboards of keyboard such as the planck Alice-layout.',
                     type: '',
                     price: '',
-                    imgUrl: '',
+                    imgUrl: 'https://i.imgur.com/HxTSAZz.jpg',
                 },
                 {
-                    name: 'Spacebars',
-                    description: '',
+                    name: 'Sculpture',
+                    description:
+                        'The Sculpture kit offers as amazing add-on that fits the theme, it offers mono greek legends for an ascetic',
                     type: '',
                     price: '',
-                    imgUrl: '',
+                    imgUrl: 'https://i.imgur.com/vh4ZHXz.jpg',
                 },
             ],
             designerName: 'Quaddepo',
