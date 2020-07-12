a258642e92131d16982fdfc0149f3ea4fb325acf
noudadrichem <info@noudadrichem.com>
2020-07-12 17:12:12 +0200

    mobile ready new filter

--- components/Filters/index.tsx
+++ components/Filters/index.tsx
@@ -12,8 +12,6 @@ import MultiSelect from '../Multiselect';
 import Select from '../Select';
 import Tab from './Tab';
 import Arrow from '../Arrow';
-import Link from 'next/link';
-import ButtonLink from '../ButtonLink';
 import Button from '../Button';
 import FilterIcon from '../FilterIcon';
 import { motion } from 'framer-motion';
@@ -29,8 +27,9 @@ function Filters(props: FiltersProps): JSX.Element {
     useEffect(function handleToggleOnWindowSize() {
         const isBrowser = typeof window !== `undefined`;
         if (isBrowser) {
-            if (window.innerWidth > 562) {
+            if (window.innerWidth > 826) {
                 setIsOpen(true);
+                setIsExtraFilterOpen(false);
             }
         }
     }, []);
@@ -79,97 +78,116 @@ function Filters(props: FiltersProps): JSX.Element {
         };
         return labelOptions[tab];
     }
+    function openMobileFilters() {
+        setIsOpen(!isOpen);
+        setIsExtraFilterOpen(!isExtraFiltersOpen);
+    }
 
     const extraFilterAnimationVariants = {
-        open: { height: 'auto' },
-        closed: { height: 0 },
+        open: {
+            height: 'auto',
+            overflow: 'initial',
+        },
+        closed: {
+            height: 0,
+            overflow: 'hidden',
+        },
     };
 
     return (
         <>
-            <div className="mobile-toggle" onClick={() => setIsOpen(!isOpen)}>
+            <div className="mobile-toggle" onClick={openMobileFilters}>
                 <h5>{isOpen ? 'Close ' : 'Open '}search filters</h5>
                 <Arrow color="#566073" size={16} direction={isOpen ? 'top' : 'bottom'} />
             </div>
 
-            <div className={`filters ${isOpen ? 'open' : 'closed'}`}>
-                <div className="left-side">
-                    <div className="more-filters open">
-                        <Button
-                            variant="secondary"
-                            size="sm"
-                            onClick={() => setIsExtraFilterOpen(!isExtraFiltersOpen)}
-                            className={`${isExtraFiltersOpen ? 'active' : ''}`}
-                        >
-                            <FilterIcon />
-                            Filters
-                        </Button>
-                    </div>
+            <motion.div
+                className={`filters-container`}
+                animate={isOpen ? 'open' : 'closed'}
+                variants={extraFilterAnimationVariants}
+            >
+                <div className={`filters ${isOpen ? 'open' : 'closed'}`}>
+                    <div className="left-side">
+                        <div className="more-filters open">
+                            <Button
+                                variant="secondary"
+                                size="sm"
+                                onClick={() => setIsExtraFilterOpen(!isExtraFiltersOpen)}
+                                className={`${isExtraFiltersOpen ? 'active' : ''} desktop-only`}
+                            >
+                                <FilterIcon />
+                                Filters
+                            </Button>
+                        </div>
 
-                    <div className="filter availability mobile-only">
-                        <Select
-                            label="Availability"
-                            name="Choose availability"
-                            onSelectChange={(selectedFilterValue) =>
-                                dispatch({
-                                    type: 'set',
-                                    payload: {
-                                        filters: {
-                                            ...state.filters,
-                                            availabilityFilter: selectedFilterValue,
+                        <div className="filter availability mobile-only">
+                            <Select
+                                label="Availability"
+                                name="Choose availability"
+                                onSelectChange={(selectedFilterValue) =>
+                                    dispatch({
+                                        type: 'set',
+                                        payload: {
+                                            filters: {
+                                                ...state.filters,
+                                                availabilityFilter: selectedFilterValue,
+                                            },
                                         },
-                                    },
-                                })
-                            }
-                            values={AVAILABILITY_OPTIONS.map((t) => ({
-                                id: t,
-                                name: getLabelByAvailability(t),
-                            }))}
-                        />
+                                    })
+                                }
+                                values={AVAILABILITY_OPTIONS.map((t) => ({
+                                    id: t,
+                                    name: getLabelByAvailability(t),
+                                }))}
+                            />
+                        </div>
                     </div>
-                </div>
 
-                <div className="centered-side">
-                    <div className="filter availability desktop-only">
-                        <div className="tabs">
-                            {AVAILABILITY_OPTIONS.map((tab: string, idx: number) => (
-                                <Tab
-                                    label={getLabelByAvailability(tab)}
-                                    type={AVAILABILITY_FILTER}
-                                    id={tab}
-                                    key={idx}
-                                />
-                            ))}
+                    <div className="centered-side">
+                        <div className="filter availability desktop-only">
+                            <div className="tabs">
+                                {AVAILABILITY_OPTIONS.map((tab: string, idx: number) => (
+                                    <Tab
+                                        label={getLabelByAvailability(tab)}
+                                        type={AVAILABILITY_FILTER}
+                                        id={tab}
+                                        key={idx}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right-side">
+                        <div className="counter">
+                            <label className="label">Keycapsets:</label>
+                            <p className="light">{state.allKeycapsetsCount}</p>
                         </div>
                     </div>
                 </div>
-                <div className="right-side">
-                    <div className="counter">
-                        <label className="label">Keycapsets:</label>
-                        <p className="light">{state.allKeycapsetsCount}</p>
+
+                <motion.div
+                    animate={isExtraFiltersOpen ? 'open' : 'closed'}
+                    variants={extraFilterAnimationVariants}
+                    className="extra-filters"
+                >
+                    <div className="filter brand">
+                        <MultiSelect isMulti label="Brand" options={BRAND_OPTIONS} onChange={handleBrandFilter} />
                     </div>
-                </div>
-            </div>
 
-            {/* {isExtraFiltersOpen && ( */}
-            <motion.div
-                animate={isExtraFiltersOpen ? 'open' : 'closed'}
-                variants={extraFilterAnimationVariants}
-                className="extra-filters"
-            >
-                <div className="filter brand">
-                    <MultiSelect isMulti label="Brand" options={BRAND_OPTIONS} onChange={handleBrandFilter} />
-                </div>
+                    <div className="filter profile">
+                        <MultiSelect isMulti label="Profile" options={PROFILE_OPTIONS} onChange={handleProfileFilter} />
+                    </div>
 
-                <div className="filter profile">
-                    <MultiSelect isMulti label="Profile" options={PROFILE_OPTIONS} onChange={handleProfileFilter} />
-                </div>
-
-                <div className="filter material">
-                    <MultiSelect isMulti label="Material" options={MATERIAL_OPTIONS} onChange={handleMaterialFilter} />
-                </div>
+                    <div className="filter material">
+                        <MultiSelect
+                            isMulti
+                            label="Material"
+                            options={MATERIAL_OPTIONS}
+                            onChange={handleMaterialFilter}
+                        />
+                    </div>
+                </motion.div>
             </motion.div>
-            {/* )} */}
         </>
     );
 }
