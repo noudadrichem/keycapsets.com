c0efab2cebaf20bfaed1442fa9c9cc50270d0805
noud adrichem <info@noudadrichem.com>
2020-11-29 12:37:10 +0100

    padding and info

--- pages/promote-your-keycapset.tsx
+++ pages/promote-your-keycapset.tsx
@@ -5,14 +5,12 @@ import withGA from 'next-ga';
 import '../assets/styles/main.scss';
 
 import Heading from '../components/Heading';
-import Footer from '../components/Footer';
-import Nav from '../components/Nav';
-import ButtonLink from '../components/ButtonLink';
 import Meta from '../components/Meta';
 import Button from '../components/Button';
 import ImageCard from '../components/ImageCard';
+import Arrow from '../components/Arrow';
 
-function Terms() {
+function PromoteYourSet() {
     const title = `Promote your keycapset the right way`;
     const description = `Get a completely themed landing page and run an interest check for your unique designed keycapset?`;
 
@@ -23,20 +21,24 @@ function Terms() {
             <div className="promote-your-set container">
                 <Heading
                     mainTitle="Promote your keycapset the right way"
-                    subTitle="Get a completely themed landing page and run an interest check for your unique designed keycapset?"
+                    subTitle="Get a completely themed landing page and run an interest check for your unique designed keycapset!"
                 >
                     <div className="button-container flex align-center">
-                        <Button size="md" variant="primary" onClick={() => console.log('-')}>
+                        <Button size="lg" variant="primary" onClick={() => console.log('-')}>
                             Start now
                         </Button>
 
-                        <Button size="md" variant="secondary" onClick={() => console.log('--')}>
+                        <Button size="lg" variant="secondary" onClick={() => console.log('--')}>
                             Contact
                         </Button>
                     </div>
                 </Heading>
 
-                <section className="flex align-center">
+                <section className="set-arrow">
+                    <Arrow color="#D4E4FA" direction="bottom" />
+                </section>
+
+                {/* <section className="flex align-center">
                     <div
                         style={{
                             width: 1008,
@@ -45,10 +47,35 @@ function Terms() {
                             borderRadius: 8,
                         }}
                     ></div>
-                </section>
+                </section> */}
 
-                <section className="">
-                    <h3>Designers before you</h3>
+                <section className="section feature-block left">
+                    <div className="grid two-column">
+                        <div
+                            style={{
+                                width: 504,
+                                height: 334,
+                                background: '#f2f2f2',
+                                borderRadius: 8,
+                            }}
+                        ></div>
+
+                        <div>
+                            <h4>A themed single page to promote your unique keycapset</h4>
+                            <p className="light">
+                                Lorem ipsum dolor sit amet. Curabitur vel volutpat erat. Suspendisse metus eros,
+                                fringilla quis dolor at, accumsan maximus ligula. Etiam varius eu odio sit amet
+                                molestie. Integer condimentum sem eu orci laoreet, vel gravida purus maximus. Mauris
+                                quam tellus, finibus eget ipsum sit amet, elementum efficitur lectus. In et quam diam.
+                                Curabitur efficitur dolor in libero hendrerit, id maximus est bibendum. Etiam ac nibh
+                                sagittis tellus sollicitudin accumsan maximus a leo.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+                {/*
+                <section className="section promo-before">
+                    <h3 className="center">Designers before you</h3>
 
                     <div className="images-container">
                         <ImageCard
@@ -89,8 +116,9 @@ function Terms() {
                         />
                     </div>
                 </section>
+*/}
 
-                <section className="feature-block left">
+                <section className="section feature-block right">
                     <div className="grid two-column">
                         <div
                             style={{
@@ -115,7 +143,7 @@ function Terms() {
                     </div>
                 </section>
 
-                <section className="feature-block right">
+                <section className="section feature-block left">
                     <div className="grid two-column">
                         <div
                             style={{
@@ -139,6 +167,8 @@ function Terms() {
                         </div>
                     </div>
                 </section>
+
+                {/* <section className="section"></section> */}
 
                 {/* <div className="text-container">
                     <h4>Use of the Service</h4>
@@ -156,4 +186,4 @@ function Terms() {
     );
 }
 
-export default withGA('UA-115865530-2', Router)(Terms);
+export default withGA('UA-115865530-2', Router)(PromoteYourSet);
