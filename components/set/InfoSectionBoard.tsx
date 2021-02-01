195d07664e4bfbc1c63f57943dfbb120c512fa97
noudadrichem <info@noudadrichem.com>
2021-02-01 16:17:25 +0100

    remove comments

--- components/set/InfoSectionBoard.tsx
+++ components/set/InfoSectionBoard.tsx
@@ -12,22 +12,6 @@ function InfoSectionBoard(props: InfoSectionBoardProps): JSX.Element {
 
     return (
         <section className="section set-info-section">
-            {/* {!keycapset.isInterestCheck && (
-                <div className="set-info-section-label">
-                    <label className="label">Start date</label>
-                    <label className="label large">
-                        {moment(keycapset.groupbuyStartDate).format('Do MMM YYYY')}
-                    </label>
-                </div>
-            )}
-            {!keycapset.isInterestCheck && (
-                <div className="set-info-section-label">
-                    <label className="label">End date</label>
-                    <label className="label large">
-                        {moment(keycapset.groupbuyEndDate).format('Do MMM YYYY')}
-                    </label>
-                </div>
-            )} */}
             <div className="set-info-section-label">
                 <label className="label">Designer</label>
                 <label className="label large">{keycapset.designerName || 'Unknown'}</label>
@@ -40,13 +24,6 @@ function InfoSectionBoard(props: InfoSectionBoardProps): JSX.Element {
                 <label className="label">Form factor</label>
                 <label className="label large">60%</label>
             </div>
-            {/* <div className="set-info-section-label">
-                <label className="label">Typing angle</label>
-                <label className="label large">
-                    5.5° typing angle <br />
-                    6.5° case slope
-                </label>
-            </div> */}
             <div className="set-info-section-label">
                 <label className="label">Manufacturer</label>
                 <label className="label large">Salvun</label>
