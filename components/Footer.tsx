1c0045219471eefe7e25ac5517dfe1a93999d482
noud adrichem <info@noudadrichem.com>
2021-06-21 12:14:00 +0200

    feature: pyp admin sync 2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Logo from './Logo';
-import useModalStore from '../hooks/useModalStore';
+import useModalStore, { Modals } from '../hooks/useModalStore';
 
 interface FooterProps {
     noSponsor?: boolean;
